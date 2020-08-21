7d1889fb42771ad3f74a9a5dcf8d08d90adcec58
Felipe Martins <felipemjesus@outlook.com>
2020-08-21 03:23:39 +0000

    Utilizando gorm

--- db/main.go
+++ db/main.go
@@ -1,24 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
-	"github.com/labstack/echo"
 )
 
 type Member struct {
 	Name string
-}
-
-func members(db *gorm.DB) func(echo.Context) error {
-	return func(c echo.Context) error {
-		var members []Member
-		db.Find(&members)
-		return c.JSON(http.StatusOK, members)
-	}
 }
 
 func main() {
@@ -28,17 +16,4 @@ func main() {
 	}
 
 	db.AutoMigrate(&Member{})
-
-	//db.Create(&Member{Name: "Felipe"})
-
-	var members []Member
-	db.Find(&members)
-
-	fmt.Println(members)
-
-	defer db.Close()
-
-	e := echo.New()
-	e.GET("/members", members(db))
-	e.Start("0.0.0.0:8080")
 }
