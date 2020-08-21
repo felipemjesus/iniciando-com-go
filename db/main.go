50ca8ac81d294f1ea589e3ef7c98d619644172c2
Felipe Martins <felipemjesus@outlook.com>
2020-08-21 03:22:52 +0000

    Utilizando o framework echo

--- db/main.go
+++ db/main.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"github.com/labstack/echo"
 )
 
 type Member struct {
 	Name string
+}
+
+func members(db *gorm.DB) func(echo.Context) error {
+	return func(c echo.Context) error {
+		var members []Member
+		db.Find(&members)
+		return c.JSON(http.StatusOK, members)
+	}
 }
 
 func main() {
@@ -27,4 +37,8 @@ func main() {
 	fmt.Println(members)
 
 	defer db.Close()
+
+	e := echo.New()
+	e.GET("/members", members(db))
+	e.Start("0.0.0.0:8080")
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/felipemjesus/iniciando-com-go
 
 go 1.15
 
-require github.com/jinzhu/gorm v1.9.16
+require (
+	github.com/jinzhu/gorm v1.9.16
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/gommon v0.3.0 // indirect
+)
